45d46b660e346db082fbdeacbd3b62ab665d2fec
김성훈(Seonghun Kim)_인턴 <sh.kim14@konai.com>
2023-09-25 10:21:08 +0900

    fix: url 재수정

--- auth.js
+++ auth.js
@@ -45,7 +45,7 @@ router.get('/callback', async (req, res) => {
     };
 
     try {
-      const tokenResponse = await axios.post(`${tokenUrl}?${new URLSearchParams(tokenParams)}`, tokenParams, {
+      const tokenResponse = await axios.post(tokenUrl, tokenParams, {
         headers: {
           'Content-Type': 'application/json'
         }
