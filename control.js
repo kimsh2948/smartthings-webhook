4f7ce57f79ea00c0752baa5a8b8c653ddf792c08
김성훈(Seonghun Kim)_인턴 <sh.kim14@konai.com>
2023-10-10 12:34:06 +0900

    feat: control 모듈 수정

--- control.js
+++ control.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const session = require('express-session');
+const axios = require('axios');
 const router = express.Router();
 
-const checkSession = () => {
+axios.defaults.baseURL = 'https://api.smartthings.com';
+
+// 환경변수로 node에서 허가되지 않은 인증TLS통신을 거부하지 않겠다고 설정
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const checkSession = (req, res) => {
     if (!req.session.accessToken) {
-        return res.redirect('/login'); // 로그인 페이지로 리다이렉션
+        return res.redirect('/oauth/login'); // 로그인 페이지로 리다이렉션
     }
 };
 
 router.get('/devices', async (req, res) => {
-    checkSession();
+    checkSession(req, res);
     const accessToken = req.session.accessToken;
 
     axios.get(`/devices`, {
