b165f9ca8a2a9b12b3e430dbddfc85f951eac456
김성훈(Seonghun Kim)_인턴 <sh.kim14@konai.com>
2023-10-12 13:03:31 +0900

    feat: 디바이스 상태 조회 UI 구현

--- public/index-script.js
+++ public/index-script.js
@@ -1,6 +1,9 @@
 // script.js
 document.addEventListener("DOMContentLoaded", () => {
     const deviceListContainer = document.querySelector('.device-list');
+    const deviceDetailsContainer = document.querySelector('.device-details');
+    const deviceImage = document.querySelector('.device-image');
+    const deviceStatus = document.querySelector('.device-status');
 
     // API에서 디바이스 목록을 가져오는 함수
     async function fetchDeviceList() {
@@ -15,11 +18,51 @@ document.addEventListener("DOMContentLoaded", () => {
             data.forEach(device => {
                 const deviceItem = document.createElement('div');
                 deviceItem.className = 'device-item';
+                deviceItem.dataset.id = device.deviceId; 
                 deviceItem.textContent = device.name;
                 deviceListContainer.appendChild(deviceItem);
             });
         } catch (error) {
             console.error('Error fetching device list:', error);
+        }
+    }
+    // 디바이스 상태조회하는 함수
+    async function fetchDeviceStatus(deviceId) {
+        try {
+            // API를 호출하여 디바이스 제어 요청
+            const response = await fetch(`https://port-0-smartthings-webhook-2rrqq2blmqxv7cr.sel5.cloudtype.app/control/devic-status?${deviceId}`, {
+                method: 'GET',
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            let imageURL = '';
+            const status = data.components.main.switch.switch.value;
+            deviceStatus.textContent = status;
+            if (status === 'on'){
+                deviceImage.src = `/image/switch-on`;
+            } else if (status === 'off'){
+                deviceImage.src = `/image/switch-off`;
+            }
+
+        } catch (error) {
+            console.error('Error controlling device:', error);
+        }
+    }
+
+    // 디바이스를 제어하는 함수
+    async function controlDevice(deviceId) {
+        try {
+            // API를 호출하여 디바이스 제어 요청
+            const response = await fetch(`https://port-0-smartthings-webhook-2rrqq2blmqxv7cr.sel5.cloudtype.app/control/devices/${deviceId}/toggle`, {
+                method: 'POST',
+            });
+
+            const data = await response.json();
+            console.log(data); // 성공한 경우에 대한 응답을 콘솔에 출력
+        } catch (error) {
+            console.error('Error controlling device:', error);
         }
     }
 
