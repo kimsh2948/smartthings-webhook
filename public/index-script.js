6d2a09be74f85255732482d88902750479fdef26
김성훈(Seonghun Kim)_인턴 <sh.kim14@konai.com>
2023-10-24 14:33:22 +0900

    fix: 수정

--- public/index-script.js
+++ public/index-script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function getCookie(name) {
         const value = `; ${document.cookie}`;
+        console.log(value);
         const parts = value.split(`; ${name}=`);
         if (parts.length === 2) return parts.pop().split(';').shift();
     }
